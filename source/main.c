a24777a24a43c512729b5bbd2553a209b759be08
reuben12358 <rdcu001@ucr.edu>
2020-01-21 14:43:48 -0800

    lab4 p2 done

--- source/main.c
+++ source/main.c
@@ -98,7 +98,7 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
 
-	BS_state = BS_tempstart;
+	BS_state = BS_start;
 
 	// unsigned char A = 0x00; // temp variable for value of A
 	// C = 0x07;
