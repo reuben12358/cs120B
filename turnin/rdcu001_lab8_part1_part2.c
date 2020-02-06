81be40ee82d805b9cb47618300e073ccbe6837d9
Reuben D'cunha <rdcu001@wch136-13.cs.ucr.edu>
2020-02-06 13:48:46 -0800

    done lab 8

--- turnin/rdcu001_lab8_part1_part2.c
+++ turnin/rdcu001_lab8_part1_part2.c
@@ -19,7 +19,7 @@ void ADC_init() {
 int main(void) {
 	// DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-	// DDRD = 0xFF; PORTD = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
 	unsigned short x;
 	unsigned char b;
 	unsigned char d;
@@ -28,17 +28,10 @@ int main(void) {
 	
 	while(1) {
 		x = ADC;
-		
-		if (x < 32)
-			b = 0;
-		
-		else
-			b = 63;
-		
-		// b = (char)x;
-		// d = (char)(x >> 8); 
+		b = (char)x;
+		d = (char)((x >> 8) & 0x03); 
 		PORTB = b;
-		// PORTD = d;
+		PORTD = d;
 		// 1) Read input
 		// 2) Perform computation
 		// 3) Write output
