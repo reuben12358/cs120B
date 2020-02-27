f3c2df4259f9b43183a7ab9f00a1a0f136743cf7
reuben12358 <rdcu001@ucr.edu>
2020-02-27 00:44:59 -0800

    start of final project

--- source/main.c
+++ source/main.c
@@ -12,45 +12,18 @@
 #include "simAVRHeader.h"
 #endif	
 
-void ADC_init() {
-	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
-}
-
 int main(void) {
-	// DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
 	// DDRD = 0xFF; PORTD = 0x00;
-	unsigned short x;
-	unsigned char b;
-	unsigned char d;
-	
-	ADC_init();
-	
+		
 	while(1) {
-		x = ADC;
-		
-		if (x > 63)
-			b = 63;
-		else if ((x <= 63) && (x > 56))	
-			b = 55;
-		else if ((x <= 55) && (x > 48))	
-			b = 47;
-		else if ((x <= 47) && (x > 40))	
-			b = 39;
-		else if ((x <= 39) && (x > 32))	
-			b = 31;
-		else if ((x <= 31) && (x > 24))	
-			b = 23;
-		else if ((x <= 23) && (x > 16))	
-			b = 15;
-		else if ((x <= 15) && (x > 8))	
-			b = 7;
-		else
-			b = 0;
-		
+	
+		if (PINA) {
+			PORTB = 0xff;
+		}	
 		// b = (char)x;
-		// d = (char)(x >> 8); 
-		PORTB = b;
+		// d = (char)(x >> 8);
 		// PORTD = d;
 		// 1) Read input
 		// 2) Perform computation
