1b75540a6a50ea6b1f639304067cffc98331d2fd
Reuben D'cunha <rdcu001@wch136-24.cs.ucr.edu>
2020-03-11 19:55:15 -0700

    project beta complete

--- source/main.c
+++ source/main.c
@@ -13,6 +13,7 @@
 #endif
 #include <util/delay.h>
 #include "nokia5110.h"
+#include <stdlib.h>
 
 // REFERENCE: melanie for snes controller code
 
@@ -134,8 +135,44 @@ int ingametimer = 0;
 int numturns = 0;
 int maxturns = 0;
 int showtimer = 0;
+int capA = 0;
+int capB = 0;
+int maxA = 0;
+int maxB = 0;
+int target = 0;
+int gamewin = 0;
 
-enum snes {gamestart, snes_wait, snes_start, snes_select , snes_a, snes_b, snes_x, snes_y, gameover } snes_state;
+void randomizer() {
+	int r = rand();
+	
+	if (r%5 == 0) {
+		maxA = 8;
+		maxB = 2;
+		target = 4;
+	}
+	else if (r%5 == 1) {
+		maxA = 5;
+		maxB = 2;
+		target = 1;
+	}
+	else if (r%5 == 2) {
+		maxA = 7;
+		maxB = 2;
+		target = 3;
+	}
+	else if (r%5 == 3) {
+		maxA = 4;
+		maxB = 1;
+		target = 3;
+	}
+	else {
+		maxA = 5;
+		maxB = 3;
+		target = 4;
+	}
+}
+
+enum snes {gamestart, snes_wait, snes_start, snes_select , snes_a, snes_b, snes_x, snes_y, gameover, choice_a, choice_b, choice_x } snes_state;
 
 void snes_tick(int state) {
 	switch(state) {
@@ -146,10 +183,13 @@ void snes_tick(int state) {
 				snes_state = gameover;
 				break;
 			}
-
+			if ((capA == target) || (capB == target)) {
+				gamewin = 1;
+				snes_state = gameover;
+				break;
+			}
 			button = GetSNESIn();
 			mapPlayerInput(button, ins);
-
 			if (inputContains(ins, Start))
 				snes_state = snes_start;
 			else if (inputContains(ins, Select))
@@ -172,14 +212,99 @@ void snes_tick(int state) {
 			snes_state = snes_wait;
 			break;		
 		case snes_a: // pour a -> b
-			snes_state = snes_wait;
+			snes_state = choice_a;
 			break;		
+		case choice_a: // choose a/b to pour into b/a
+			if (ingametimer >= 60) {
+				snes_state = gameover;
+				break;
+			}
+			button = GetSNESIn();
+			mapPlayerInput(button, ins);
+			if (inputContains(ins, A)) {
+				while ((capA < maxA) && (capB > 0)) {
+					capA++;
+					capB--;
+				}
+				ingametimer = 0;
+				snes_state = snes_wait;
+			}
+			else if (inputContains(ins, B)) {
+				while ((capA < maxA) && (capB > 0)) {
+					capA++;
+					capB--;
+				}
+				while ((capB < maxB) && (capA > 0)) {
+					capA--;
+					capB++;
+				}
+				ingametimer = 0;
+				snes_state = snes_wait;
+			}
+			else if (inputContains(ins, X)) {
+				snes_state = snes_wait;
+				break;
+			}
+			else {
+				snes_state = choice_a;
+			}
+			break;
 		case snes_b: // empty a/b
-			snes_state = snes_wait;
+			snes_state = choice_b;
 			break;		
+		case choice_b: // choose a,b to empty
+			if (ingametimer >= 60) {
+				snes_state = gameover;
+				break;
+			}
+			button = GetSNESIn();
+			mapPlayerInput(button, ins);
+			if (inputContains(ins, A)) {
+				capA = 0;
+				ingametimer = 0;
+				snes_state = snes_wait;
+			}
+			else if (inputContains(ins, B)) {
+				capB = 0;
+				ingametimer = 0;
+				snes_state = snes_wait;
+			}
+			else if (inputContains(ins, X)) {
+				snes_state = snes_wait;
+				break;
+			}
+			else {
+				snes_state = choice_b;
+			}
+			break;
 		case snes_x: // fill a/b
-			snes_state = snes_wait;
+			snes_state = choice_x;
 			break;		
+		case choice_x: // choose a,b to fill
+			if (ingametimer >= 60) {
+				snes_state = gameover;
+				break;
+			}
+			button = GetSNESIn();
+			mapPlayerInput(button, ins);
+			if (inputContains(ins, A)) {
+				capA = maxA;
+				ingametimer = 0;
+				snes_state = snes_wait;
+			}
+			else if (inputContains(ins, B)) {
+				capB = maxB;
+				ingametimer = 0;
+				snes_state = snes_wait;
+			}
+			else if (inputContains(ins, X)) {
+				snes_state = snes_wait;
+				break;
+			}
+			else {
+				snes_state = choice_x;
+			}
+			break;
 		case snes_y: // show timer
 			snes_state = snes_wait;
 			break;	
@@ -199,54 +324,129 @@ void snes_tick(int state) {
 			mapPlayerInput(button, ins);
 
 			if (inputContains(ins, A)) {
-				maxturns = 999;
+				maxturns = 80;
 				snes_state = snes_wait;
 			}
 			else if (inputContains(ins, B)) {
-				maxturns = 20;
+				maxturns = 40;
 				snes_state = snes_wait;
 			}
 			else if (inputContains(ins, X)) {
-				maxturns = 10;
+				maxturns = 20;
 				snes_state = snes_wait;
 			}
-			else 
+			else {
 				snes_state = gamestart;
-			
+			}
+			gamewin = 0;
+			capA = 0;
+			capB = 0;
+			randomizer();
 			ingametimer = 0;
 			numturns = 0;
 			break;
 		case snes_wait:
 			nokia_lcd_clear();
-			if (showtimer) {
-				nokia_lcd_write_char((char)ingametimer/10 + 0x30,3);
-				nokia_lcd_write_char((char)ingametimer%10 + 0x30,3);
+			nokia_lcd_write_string("Jug A = ",1);
+			nokia_lcd_write_char((char)capA + 0x30,1);
+			nokia_lcd_write_char(0x2f,1);
+			nokia_lcd_write_char((char)maxA + 0x30,1);
+			nokia_lcd_write_string("   Jug B = ",1);			
+			nokia_lcd_write_char((char)capB + 0x30,1);
+			nokia_lcd_write_char(0x2f,1);
+			nokia_lcd_write_char((char)maxB + 0x30,1);
+			
+			nokia_lcd_write_string("   Target = ",1);
+			nokia_lcd_write_char((char)target + 0x30,1);			
+			
+			if (!showtimer) {
+				nokia_lcd_write_string("    Timer = ",1);
+				nokia_lcd_write_char((char)ingametimer/10 + 0x30,1);
+				nokia_lcd_write_char((char)ingametimer%10 + 0x30,1);
 			}
+			else {
+				nokia_lcd_write_string("              ",1);
+			}
+			
+			nokia_lcd_write_string("    Select Action A,B,X,Y,St,Se",1);
 			ingametimer++;
 			break;
 		case snes_start:
 			ingametimer = 0;
 			numturns = 0;
-			
-			
+			maxturns = 0;
+			capA = 0;
+			capB = 0;
+			maxA = 0;
+			maxB = 0;
 			break;
 		case snes_select:
 			numturns = 0;
 			ingametimer = 0;
+			capA = 0;
+			capB = 0;
 			break;
-		case snes_a:
+		case snes_a: // pour a -> b
 			numturns++;
 			ingametimer = 0;
 			break;
-		case snes_b:
+		case choice_a:
+			nokia_lcd_clear();
+			nokia_lcd_write_string("choose jug to pour into A/B ", 1);
+			if (!showtimer) {
+				nokia_lcd_write_string("Timer = ",1);
+				nokia_lcd_write_char((char)ingametimer/10 + 0x30,1);
+				nokia_lcd_write_char((char)ingametimer%10 + 0x30,1);
+			}
+			else {
+				nokia_lcd_write_string("              ",1);
+			}
+			nokia_lcd_write_string("                  ",1);
+			nokia_lcd_write_string("X to go back",1);
+			
+			ingametimer++;
+			break;
+		case snes_b: // empty a/b
 			numturns++;
 			ingametimer = 0;
 			break;
-		case snes_x:
+		case choice_b:
+			nokia_lcd_clear();
+			nokia_lcd_write_string("choose jug to empty A/B ", 1);
+			if (!showtimer) {
+				nokia_lcd_write_string("    Timer = ",1);
+				nokia_lcd_write_char((char)ingametimer/10 + 0x30,1);
+				nokia_lcd_write_char((char)ingametimer%10 + 0x30,1);
+			}
+			else {
+				nokia_lcd_write_string("              ",1);
+			}
+			nokia_lcd_write_string("                  ",1);
+			nokia_lcd_write_string("X to go back",1);
+			
+			ingametimer++;
+			break;
+		case snes_x: // fill a/b
 			numturns++;
 			ingametimer = 0;
 			break;
-		case snes_y:
+		case choice_x:
+			nokia_lcd_clear();
+			nokia_lcd_write_string("choose jug to fill A/B  ", 1);
+			if (!showtimer) {
+				nokia_lcd_write_string("    Timer = ",1);
+				nokia_lcd_write_char((char)ingametimer/10 + 0x30,1);
+				nokia_lcd_write_char((char)ingametimer%10 + 0x30,1);
+			}
+			else {
+				nokia_lcd_write_string("              ",1);
+			}
+			nokia_lcd_write_string("                  ",1);
+			nokia_lcd_write_string("X to go back",1);
+			
+			ingametimer++;
+			break;
+		case snes_y: // toggle timer display
 			if (!showtimer)
 				showtimer++;
 			else
@@ -254,7 +454,10 @@ void snes_tick(int state) {
 			break;
 		case gameover:
 			nokia_lcd_clear();
-			nokia_lcd_write_string("Gameover",2);
+			if (!gamewin) 
+				nokia_lcd_write_string("Gameover",2);
+			else 
+				nokia_lcd_write_string("Congrats",2);
 			nokia_lcd_write_string("Thank you for playing! PressSELECT 2 reset",1);
 
 			button = GetSNESIn();
@@ -268,6 +471,10 @@ void snes_tick(int state) {
 			ingametimer = 0;
 			numturns = 0;
 			maxturns = 0;
+			capA = 0;
+			capB = 0;
+			maxA = 0;
+			maxB = 0;
 			break;
 	}
 }
