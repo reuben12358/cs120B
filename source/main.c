48721b8647f7b4f9c40101936d2dc9d835eb99df
reuben12358 <rdcu001@ucr.edu>
2020-03-15 20:46:54 -0700

    last time im opening this bullshit

--- source/main.c
+++ source/main.c
@@ -1,7 +1,6 @@
-/*	Author: lsong013
- *  Partner(s) Name: Reuben D'cunha
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+/*	Author: Reuben D'cunha
+ *	Lab Section: 22
+ *	Assignment: Custom Lab Project
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -16,43 +15,6 @@
 #include <stdlib.h>
 
 // REFERENCE: melanie for snes controller code
-
-/*
-volatile unsigned char TimerFlag = 0;
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
-void TimerOn()
-{
-	TCCR1B = 0x0B;
-	OCR1A = 125;
-	TIMSK1 = 0x02;
-	TCNT1 = 0;
-	_avr_timer_cntcurr = _avr_timer_M;
-	SREG |= 0x80;
-}
-void TimerOff()
-{
-	TCCR1B = 0x00;
-}
-void TimerISR()
-{
-	TimerFlag = 1;
-}
-ISR(TIMER1_COMPA_vect)
-{
-	_avr_timer_cntcurr--;
-	if (_avr_timer_cntcurr == 0)
-	{
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
-	}
-}
-void TimerSet(unsigned long M)
-{
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-}
-*/
 
 unsigned char SetBit(unsigned char pin, unsigned char number, unsigned char bin_value) 
 {
@@ -485,6 +447,7 @@ int main(void) {
 
 	// unsigned char output = 0x00;
 
+	// reference giuseppi for lcd code
 	nokia_lcd_init();
 	nokia_lcd_clear();
 	
